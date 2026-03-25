ab6e3a20b486baedd21d8b1da466b922da95aba7
DanBan <166554933+Obsidian500@users.noreply.github.com>
2026-03-25 16:59:48 +0000

    Refactor test parameters for location QC tests

--- tests/test_impossible_location_qc.py
+++ tests/test_impossible_location_qc.py
@@ -6,18 +6,6 @@ from unittest.mock import patch
 from toolbox.steps.custom.qc.impossible_location_qc import impossible_location_qc
 from utils.test_utils import create_mock_dataset
 
-TEST_GOOD_LATS = [45.0, -89.9]
-TEST_GOOD_LONS = [179.9, -179.9]
-
-TEST_BAD_LATS = [95.0, -100.0] 
-TEST_BAD_LONS = [185.0, -185.0]
-
-TEST_NAN_LATS = [np.nan, 45.0]
-TEST_NAN_LONS = [10.0, np.nan]
-
-TEST_ZERO_LATS = [0.0, 0.0]
-TEST_ZERO_LONS = [0.0, 0.0]
-
 
 def test_missing_variables():
     data = xr.Dataset({"TEMP": ("N_MEASUREMENTS", [10.0, 12.0])})
@@ -27,11 +15,11 @@ def test_missing_variables():
         qc_step.return_qc()
 
 
-@pytest.mark.parametrize("lats, lons, expected_lat, expected_lon", [
-    (TEST_GOOD_LATS, TEST_GOOD_LONS, [1, 1], [1, 1]),
-    (TEST_ZERO_LATS, TEST_ZERO_LONS, [1, 1], [1, 1]),
-    (TEST_BAD_LATS, TEST_BAD_LONS, [4, 4], [4, 4]),
-    (TEST_NAN_LATS, TEST_NAN_LONS, [9, 1], [1, 9]),
+@pytest.mark.parametrize("lats,lons,lat_flags,lon_flags", [
+    ([45.0, -89.9], [179.9, -179.9], [1, 1],  [1, 1]),  # valid
+    ([0.0, 0.0],    [0.0, 0.0],      [1, 1],   [1, 1]),  # zero
+    ([95.0, -100.0],[185.0, -185.0], [4, 4],   [4, 4]),  # invalid
+    ([np.nan, 45.0],[10.0, np.nan],  [9, 1],   [1, 9]),  # nan
 ])
 def test_locations(lats, lons, expected_lat, expected_lon):
     data = create_mock_dataset(lats=lats, lons=lons)
